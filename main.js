8daa0e8a2ad17ef82a15147aa382b83d598d01a9
GabrielDSRodrigues <gabrielrodrigues.sp.br@gmail.com>
2021-06-18 23:24:12 -0300

    Adição de caixa com a lista dos pedidos junto de seus valores e quantidades

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 function id(e) { return document.getElementById(e); }
 
-function soma(e, total, pos) {
-  let value = e.value;
-  e.value = parseInt(value) + 1;
-  total.innerText = "Total R$" + (parseFloat(itens[pos].valor) * e.value);
-}
-
-function subtrai(e, total, pos) {
-  let value = e.value;
-  if (value > 0) {
-    e.value = parseInt(value) - 1;
-  }
-  total.innerText = "Total R$" + (parseFloat(itens[pos].valor) * e.value);
-}
 
 class cardapioItem {
   nome;
@@ -20,8 +7,100 @@ class cardapioItem {
   ingredientes;
 }
 
+class pedido {
+  quantidade;
+  nome;
+  valorUN;
+  valor() { return (this.valorUN * this.quantidade); }
+}
+
+
+let pop = id("pop");
+pop.style.display = "none";
+
+let ldp = id("listaDePedidos");
+
 let ids = 0;
 let itens = [];
+let itensPedidos = [];
+
+
+id("btnPedir").addEventListener("click", () => {
+  let popSD = pop.style.display;
+  if (popSD == "block") {
+    pop.style.display = "none";
+  } else if (popSD == "none") {
+    pop.style.display = "block";
+    pop.style.left = ((document.body.offsetWidth / 2) - pop.offsetWidth / 2) + "px";
+    pop.style.top = ((document.body.offsetHeight / 2) - pop.offsetHeight / 2) + "px";
+  }
+});
+
+
+function pedidosAtl() {
+  ldp.innerHTML = "";
+  for (let i = 0; i < itensPedidos.length; i++) {
+    ldp.insertAdjacentHTML("beforeend", `
+      <li>
+        <p><b>${itensPedidos[i].quantidade}</b>-${itensPedidos[i].nome} <b>R$${itensPedidos[i].valor()}</b></p>
+      </li>
+    `);
+  }
+}
+
+function addPedido(qnt, nome, valor) {
+  let jaAdd = false;
+  let pos;
+  for (let i = 0; i < itensPedidos.length; i++) {
+    if (itensPedidos[i].nome == nome) {
+      jaAdd = true;
+      pos = i;
+    }
+  }
+  if (jaAdd == false) {
+    let novo = new pedido();
+    novo.quantidade = qnt;
+    novo.nome = nome;
+    novo.valorUN = valor;
+    itensPedidos.push(novo);
+  } else if (jaAdd == true) {
+    itensPedidos[pos].quantidade++;
+  }
+  pedidosAtl();
+}
+
+function rmvPedido(iten, nome) {
+  let jaAdd = false;
+  let pos;
+  for (let i = 0; i < itensPedidos.length; i++) {
+    if (itensPedidos[i].nome == nome) {
+      jaAdd = true;
+      pos = i;
+    }
+  }
+  if (jaAdd == false) {
+    itensPedidos.splice(iten, 1);
+  } else if (jaAdd == true) {
+    itensPedidos[pos].quantidade--;
+  }
+  pedidosAtl();
+}
+
+function soma(e, total, pos, nome, valor) {
+  let value = e.value;
+  e.value = parseInt(value) + 1;
+  addPedido(parseFloat(e.value), nome, valor);
+  total.innerText = "Total R$" + (parseFloat(itens[pos].valor) * e.value);
+}
+
+function subtrai(e, total, pos, nome) {
+  let value = e.value;
+  if (value > 0) {
+    e.value = parseInt(value) - 1;
+    rmvPedido(parseInt(e.value), nome);
+    total.innerText = "Total R$" + (parseFloat(itens[pos].valor) * e.value)
+  }
+}
 
 function addItem(nome, valor, ingredientes, img, categoria) {
   let novo = new cardapioItem;
@@ -45,9 +124,9 @@ function addItem(nome, valor, ingredientes, img, categoria) {
     </div>
     <div class="linha">
       <div class="btnMmN">
-        <button onClick="subtrai(item${ids}, total${ids}, ${ids})">-</button>
+        <button onClick="subtrai(item${ids}, total${ids}, ${ids}, '${nome}')">-</button>
         <input type="number" value="0" id="item${ids}">
-        <button onClick="soma(item${ids}, total${ids}, ${ids})">+</button>
+        <button onClick="soma(item${ids}, total${ids}, ${ids}, '${nome}', '${valor}')">+</button>
       </div>
       <h5 id="total${ids}">Total R$0</h5>
     </div>
